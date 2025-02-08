fb31cd3b673e114662a980c0a871e6896f6dfd22
Jessika-Andrade <jessika_andrade_11@hotmail.com>
2025-02-08 10:31:30 -0300

    atualização aulas 15 e 16 e exercícios

---
+++ aula 15/atividade.js
@@ -0,0 +1,6 @@
+let num = [5, 8, 2, 9, 7, 4];
+num.push(1);
+num.sort();
+console.log(num);
+console.log(`O número de de elementos é:  ${num.length}`);
+console.log(`O primeiro número é: ${num[0]}`)
---
+++ aula 16/funcao02.js
@@ -0,0 +1,5 @@
+//Colocando o =0, informa que se não for colocado um valor no parâmetro, ele receberá 0.
+function soma(n1=0, n2=0) { 
+    return n1 + n2;
+}
+console.log(soma(4,5))
